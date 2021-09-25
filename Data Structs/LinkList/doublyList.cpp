981fd80d9889597025a6799d7b5f1366fe0f1430
Angelo <58501267+Mitochondrion-x@users.noreply.github.com>
2021-09-25 14:23:07 +0800

    Create doublyList.cpp

---
+++ Data Structs/LinkList/doublyList.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+typedef struct DNode {
+	Elemtype *data;
+	struct DNode *prior,*next;
+}DNode,*DLinkList;
+
+bool insertDNode(DLinkList &L, int i, ElemType e) {
+	DLinkList *s,*p;
+	if(i<1 || i>length(L)+1) 
+		return false;
+		
+	for(; ;) {
+		
+ 		/* 在p之后，插入s*/
+		s->next = p->next;
+		p->next->prior = s;
+		s->prior = p;
+		p->next = s;
+	}
+}
+
+bool deleteDNode(DLinkList &L, int i, Elemtype *e) {
+	DLinkList *p,*q;
+	if(i<1 || i>length(L)+1) 
+		return false;
+		
+	for(; ;) {
+		/* 删除p的后继结点q*/
+//		p->next = q->next;
+//		q->next->prior = p;
+//		free(q); 
+
+		p->prior->next = p->next;
+	 	p->next->prior = p->prior;
+	 	free(q);
+	}
+}
+
+
+
+
+
+
+
+
+
